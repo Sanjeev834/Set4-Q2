3d8f17c1164edb7f6915610e31dc26cfdfada52f
Sanjeev834 <152133782+Sanjeev834@users.noreply.github.com>
2023-11-27 12:34:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
 
+provider "google" {
+  credentials = file("<CREDENTIALS_FILE.json>")
+  project     = var.project_id
+  region      = var.region
+}
+
+module "pubsub_topic" {
+  source      = "./pubsub-module"
+  project_id  = var.project_id
+  topic_name  = var.topic_name
+  labels      = var.labels
+}
