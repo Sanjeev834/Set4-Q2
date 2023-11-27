ad42baa7d857c115cd997aa04b272284e436d57b
Sanjeev834 <152133782+Sanjeev834@users.noreply.github.com>
2023-11-27 12:38:01 +0530

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -1,1 +1,18 @@
+variable "project_id" {
+  description = "The ID of the project."
+}
 
+variable "region" {
+  description = "The region in which resources will be provisioned."
+  default     = "us-central1"
+}
+
+variable "topic_name" {
+  description = "The name of the Pub/Sub topic."
+}
+
+variable "labels" {
+  description = "A set of key/value label ."
+  type        = map(string)
+  default     = {}
+}
